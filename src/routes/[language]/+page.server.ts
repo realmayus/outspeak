177d4504beb2b1429c1c15027e050b549d1e34cf
realmayus <realmayus@gmail.com>
2022-08-30 20:27:50 +0200

    Initial Commit

--- src/routes/[language]/+page.server.ts
+++ src/routes/[language]/+page.server.ts
@@ -1,11 +1,11 @@
-import { iso6393 } from "iso-639-3";
+// import { iso6393 } from "iso-639-3";
 import db from "$lib/database";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
-    const languageFriendly = iso6393.find((x) => x.iso6393 === params.language)?.name;
-
+    // const languageFriendly = iso6393.find((x) => x.iso6393 === params.language)?.name;
+    const languageFriendly = "Japanese";
     const allPronunciations = await db.prisma.pronunciation.findMany({
         include: {
             author: { select: { username: true } },
